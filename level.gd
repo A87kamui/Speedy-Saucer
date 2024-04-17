7fe920c85a8c06452183adeb82dd842b94f51b54
Adrian Mallari <a87kamui@gmail.com>
2024-04-17 00:12:26 -0400

    Added escape key press to quit game Added quit input action

--- level.gd
+++ level.gd
@@ -4,4 +4,7 @@ extends Node2D
 func _on_maze_body_exited(body):
 	get_tree().reload_current_scene()
 
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
